3ab1d0b8efb3114cb02b1a493c8d4e0a7e012417
Viktoria Gutyrchick <vikgutyrchick@gmail.com>
2024-02-12 12:15:18 -0800

    Initial Commit

---
+++ Fetch Rewards Tests/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fetch Rewards Tests
+//
+//  Created by Viktoria Puchko on 12.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
